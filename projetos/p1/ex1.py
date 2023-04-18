e51cf676d7f5056a0d81f7a0c55c5b0e49e16dd6
MaykeNeves <102259629+MaykeNeves@users.noreply.github.com>
2023-04-18 19:23:00 +0000

    18/04 Dia 1

---
+++ projetos/p1/ex1.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print("Hello My friend!")
+
+#2. Ask the user for the city that they grew up in.
+city = input("Qual o nome da cidade em que você nasceu?\n")
+#3. Ask the user for the name of a pet.
+pet = input("Qual o nome do seu cachorro?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+combinacao = city + pet
+print("o nome da sua banda poderia ser: ",combinacao)
+#5. Make sure the input cursor shows on a new line:
+
